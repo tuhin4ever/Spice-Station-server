1c520cdfb268a615efc82c557bec17a311bd2318
tuhin_4ever <tuhin4ever19@gmail.com>
2023-05-04 13:47:22 +0600

    update all data

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
+app.use(cors());
+const chef = require("./data/data.json");
+app.get("/", (req, res) => {
+  res.send("Splice station");
+});
+app.get("/chef", (req, res) => {
+  res.send(chef);
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/chef/:id", (req, res) => {
+  const id = req.params.id;
+  const chefId = chef.find((chef) => chef.id == id);
+  res.send(chefId);
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
-
+  console.log(`Example app listening on port ${port}`);
+});
